78c04b3503dffebfbc3075d721cafbc74d8d9803
板本　耕輔 <kosuke.723723@gmail.com>
2021-07-26 13:58:40 +0900

    パスワード再設定処理

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,48 @@
-@extends('layouts.app')
+@extends('layouts.app_only_content')
 
-@section('content')
+@section("title")
+パスワードリセットリクエスト
+@endsection
+
+@section("content")
+<div class="container">
+    <div class="card" style="width: 500px">
+        <div class="card-body">
+            <div class="font-weight-bold text-center border-bottom pb-3" style="font-size: 24px">パスワードをお忘れの方</div>
+
+            <form method="POST" action="{{ route('password.email') }}" class="p-5">
+                @if (session("status"))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                @csrf
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="melpit@example.com">
+
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group mb-0">
+                    <button type="submit" class="btn btn-block btn-secondary">
+                        送信する
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+
+
+{{-- @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -44,4 +86,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
