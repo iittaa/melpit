c7f1631b920978b622335eaa4f7b415cc90039c3
板本　耕輔 <kosuke.723723@gmail.com>
2021-07-27 17:54:57 +0900

    N+1問題対策

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -11,7 +11,13 @@ class SellController extends Controller
     public function showSellForm()
     {
         $conditions = ItemCondition::orderBy("sort_no")->get();
-        $categories = PrimaryCategory::orderBY("sort_no")->get();
+        // $categories = PrimaryCategory::orderBY("sort_no")->get();
+        $categories = PrimaryCategory::query()->with([
+            "secondaryCategories" => function ($query) {
+                $query->orderBy("sort_no");
+            }
+        ])->orderBy("sort_no")->get();
+
         return view("sell", compact("conditions", "categories"));
     }
 }
