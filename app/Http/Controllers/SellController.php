1dc87474410ee0a7c62988fb8333b1b82f3e1353
板本　耕輔 <kosuke.723723@gmail.com>
2021-07-27 16:52:12 +0900

    セレクトボックス実装(商品の状態)

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ItemCondition;
 
 class SellController extends Controller
 {
     public function showSellForm()
     {
-        return view("sell");
+        $conditions = ItemCondition::orderBy("sort_no")->get();
+        return view("sell", compact("conditions"));
     }
 }
